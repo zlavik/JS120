55340784e917e9dfa43882e3b48c4a10f6bb1650
Zlavik <kivalz@live.com>
2020-09-26 09:53:55 -0700

    added lessons

---
+++ exercises/nameConstructor.js
@@ -0,0 +1,15 @@
+// Update the following code so that, instead of logging the values,
+//  each statement logs the name of the constructor to which it belongs.
+
+
+
+
+
+console.log(typeof "Hello");
+console.log(typeof [1,2,3]);
+console.log(typeof {name: 'Srdjan'});
+
+//Expected output:
+// String
+// Array
+// Object
